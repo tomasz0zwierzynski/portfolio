e17e82a8d225c2752ee056d3c7440f470e2946d6
Tomek <tomasz0zwierzynski@gmail.com>
2019-10-18 17:32:49 +0200

    dodano druga galerie

--- resources/js/cv.js
+++ resources/js/cv.js
@@ -26,3 +26,32 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " actived";
 }
+
+var slideIndex0 = 1;
+showSlides0(slideIndex0);
+
+// Next/previous controls
+function plusSlides0(n) {
+  showSlides0(slideIndex0 += n);
+}
+
+// Thumbnail image controls
+function currentSlide0(n) {
+  showSlides0(slideIndex0 = n);
+}
+
+function showSlides0(n) {
+  var i0;
+  var slides0 = document.getElementsByClassName("my-slides-0");
+  var dots0 = document.getElementsByClassName("dot-0");
+  if (n > slides0.length) {slideIndex0 = 1}
+  if (n < 1) {slideIndex0 = slides0.length}
+  for (i0 = 0; i0 < slides0.length; i0++) {
+      slides0[i0].style.display = "none";
+  }
+  for (i0 = 0; i0 < dots0.length; i0++) {
+      dots0[i0].className = dots0[i0].className.replace(" actived", "");
+  }
+  slides0[slideIndex0-1].style.display = "block";
+  dots0[slideIndex0-1].className += " actived";
+}
