c47a02005c8f934a10ca8ced043ec81ee0f75d16
Tomek <tomasz0zwierzynski@gmail.com>
2020-09-03 12:39:58 +0200

    dodano podział na podstrony

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const path = require('path');
 
+const fs = require('fs');
+const util = require('util');
+const logFile = fs.createWriteStream(__dirname + '/debug.log', {flags: 'w'});
+
 const app = express();
 const port = 3000;
+
+function log(msg) {
+    const now = new Date().toISOString();
+    logFile.write(now + ' - ' + util.format(msg) + '\n');
+}
 
 app.use( express.static( __dirname + '/resources' ) );
 app.use( express.urlencoded( {
@@ -11,10 +20,12 @@ app.use( express.urlencoded( {
 app.use( express.json() );
 
 app.get('/', (req, res) => {
+    log('GET / ' + req.ip);
     res.sendFile( path.join( __dirname, 'resources', 'cv.html' ) );
 } );
 
 app.get('/cv', (req, res) => {
+    log('GET /cv ' + req.ip);
     res.sendFile( path.join( __dirname, 'resources', 'cv.html' ) );
 } );
 
